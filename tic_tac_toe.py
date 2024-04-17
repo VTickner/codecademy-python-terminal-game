ed986e9469f67d97c0e84bee82caa85afe9a26b9
VTickner <127106836+VTickner@users.noreply.github.com>
2024-04-17 23:27:05 +0100

    class Player added
    - refactored program to use class instances for the two players
    - class Player uses:
      - __init__() to get names of players and asks which marker 1st player wants to use
      - get_marker() to error handle input for marker and set player1. marker to input

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -8,6 +8,24 @@ board_position = {1: [0, 0], 2: [0, 1], 3: [0, 2], 4: [1, 0], 5: [1, 1], 6: [1, 
 won_game = False
 board_full = False
 
+class Player:
+    def __init__(self, player_number):
+        self.name = input(f"Please enter name for player #{player_number}: ")
+        self.name = self.name.title()
+        if player_number == 1:
+            self.marker = self.get_marker()
+
+    def __repr__(self):
+        pass
+
+    def get_marker(self):
+        self.marker = input("Please enter which marker you want to use (X or O): ")
+        self.marker = self.marker.upper()
+        if self.marker not in ["X", "O"]:
+            print("Marker Error: Marker should be either X or O")
+            return self.get_marker()
+        return self.marker
+
 def game_rules():
     print("\nWelcome to the Tic-Tac-Toe Game!\n")
     print("This is a 2 player game, with each player taking turns to place their alloted marker (either X or O) on the board.")
@@ -26,60 +44,41 @@ def print_game_board():
     print("\u2500\u2500\u2500\u253c\u2500\u2500\u2500\u253c\u2500\u2500\u2500")
     print(f" {game_board[2][0]} \u2502 {game_board[2][1]} \u2502 {game_board[2][2]}")
 
-def get_marker():
-    player_marker = input("Please enter which marker you want to use (X or O): ")
-    player_marker = player_marker.upper()
-    if player_marker not in ["X", "O"]:
-        print("Marker Error: Marker should be either X or O")
-        return get_marker()
-    return player_marker
-
-def players():
-    player1_name = input("Please enter name for player #1: ")
-    player1_name = player1_name.title()
-    player1_marker = get_marker()
-    if player1_marker == "X":
-        player2_marker = "O"
-    else:
-        player2_marker = "X"
-    player2_name = input("Please enter name for player #2: ")
-    player2_name = player2_name.title()
-
-    return (player1_name, player1_marker, player2_name, player2_marker)
-    # print(player1_name + ": " + player1_marker) # test input
-    # print(player2_name + ": " + player2_marker) # test input
-
-def input_marker(count, player1_name, player1_marker, player2_name, player2_marker):
+def input_marker(count, player1, player2):
     if count % 2 != 0:
-        input_message = f"Which empty space would you like to place your {player1_marker}, {player1_name}: "
+        input_message = f"Which empty space would you like to place your {player1.marker}, {player1.name}: "
     else:
-        input_message = f"Which empty space would you like to place your {player2_marker}, {player2_name}: "
+        input_message = f"Which empty space would you like to place your {player2.marker}, {player2.name}: "
     player_turn = int(input(input_message))
     if player_turn in board_position.keys():
         x_position = board_position[player_turn][0]
         y_position = board_position[player_turn][1]
         if game_board[x_position][y_position] == " ":
             if count % 2 != 0:
-                game_board[x_position][y_position] = player1_marker
+                game_board[x_position][y_position] = player1.marker
             else:
-                game_board[x_position][y_position] = player2_marker
+                game_board[x_position][y_position] = player2.marker
         else:
             print("Position Error: Position already filled")
             # TODO: check if board is completely full
-            input_marker(count, player1_name, player1_marker, player2_name, player2_marker)
+            input_marker(count, player1, player2)
         print_game_board()
     else:
         print("Position Error: Number needs to be between 1 to 9.")    
 
 def play_game():
-    player1_name, player1_marker, player2_name, player2_marker = players()
+    player1 = Player(1)
+    player2 = Player(2)
+    if player1.marker == "X":
+        player2.marker = "O"
+    else:
+        player2.marker = "X"
     count = 1
     # TODO: while won_game = False or each space in game_board != " " continue playing game
     while won_game == False and board_full == False:
-        input_marker(count, player1_name, player1_marker, player2_name, player2_marker)
+        input_marker(count, player1, player2)
         count += 1
 
 game_rules()
 play_game()
 # print_game_board()
-
